d85d991b4d9724106f75eb5279b29ff4923b0fe0
Christian Ledgard <christianledgard@gmail.com>
2023-11-05 14:08:39 +0100

    Update _cover.tsx

--- components/Landing/_cover.tsx
+++ components/Landing/_cover.tsx
@@ -13,11 +13,11 @@ const Cover = () => {
         </div>
         <div className="item-body space-y-2 prose dark:prose-invert">
           <p>
-            I'm Christian 👋, a passionate computer scientist driven to create
-            software systems that genuinely impact people's lives. 🚀
+            {`I'm Christian 👋, a passionate computer scientist driven to create
+            software systems that genuinely impact people's lives. 🚀`}
           </p>
           <p>
-            Currently, I work at{" "}
+            {`Currently, I work at `}
             <a
               className="text-white border dark:border-neutral-700 rounded p-1 mx-1 text-sm no-underline bg-[#3c3c3c]"
               href="https://www.tesla.com"
@@ -31,11 +31,11 @@ const Cover = () => {
                 className="inline-block mr-1 my-0 align-baseline"
               />
               <span>Tesla</span>
-            </a>{" "}
-            as a Software Quality Assurance Intern. My enthusiasm for
+            </a>
+            {` as a Software Quality Assurance Intern. My enthusiasm for
             sustainable energy and the opportunity to collaborate with an
             exceptional team aligns perfectly with my eagerness to learn and
-            grow. In addition, I am also the proud founder of{" "}
+            grow. In addition, I am also the proud founder of `}
             <a
               className=" whitespace-nowrap text-white border dark:border-neutral-700 rounded p-1 mx-1 text-sm no-underline bg-[#3c3c3c] "
               href="https://www.atlastechhub.com/"
@@ -50,14 +50,14 @@ const Cover = () => {
               />
               <span className="align-middle">Atlas Tech Hub</span>
             </a>
-            {", "}a company committed to providing rapid, high-impact project
+            {`, a company committed to providing rapid, high-impact project
             delivery to clients while upholding the highest code quality
-            standards, all at an affordable price.
+            standards, all at an affordable price.`}
           </p>
           <p>
-            I strongly believe in the power of effective teamwork and
+            {`I strongly believe in the power of effective teamwork and
             communication. My journey as a computer scientist continues to be a
-            thrilling and fulfilling adventure.
+            thrilling and fulfilling adventure.`}
           </p>
         </div>
       </div>
